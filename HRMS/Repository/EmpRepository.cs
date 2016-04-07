12ff2eb793a0c3f5a3c71561dcc173bce49c9cc8
developerd2k20 <developer.2k20@gmail.com>
2016-04-07 17:40:11 +0530

    commit

--- HRMS/Repository/EmpRepository.cs
+++ HRMS/Repository/EmpRepository.cs
@@ -12,7 +12,7 @@ namespace HRMS.Repository
     public class EmpRepository
     {
         private SqlConnection con;
-        //To Handle connection related activities
+        //To Handle connection related activities test by arvind
         private void connection()
         {
             string constr = ConfigurationManager.ConnectionStrings["getconn"].ToString();
